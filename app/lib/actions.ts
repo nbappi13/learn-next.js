99eae41355bfcbec1c00dd09374d1284104a8233
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-06-28 18:21:29 +0600

    Passed the id to the Server Action to update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,9 +5,7 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+//  Step 1:FormSchema define 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -16,21 +14,44 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Step 2: omit then Create and Update 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
- 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+//  Step 3: DB connect
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+//  Step 4: Actions
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-
-    });
+  });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/Invoices');
+  redirect('/dashboard/Invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
 
   revalidatePath('/dashboard/Invoices');
