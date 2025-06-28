4ec116fb9c6e96987c3d4f851dd7f0f947a02872
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-06-28 12:38:13 +0600

    implemented debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 export default function Search() {
@@ -9,15 +10,17 @@ export default function Search() {
    const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-     if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
+  const handleSearch = useDebouncedCallback((term) => {
+  console.log(`Searching... ${term}`);
+ 
+  const params = new URLSearchParams(searchParams);
+  if (term) {
+    params.set('query', term);
+  } else {
+    params.delete('query');
   }
+  replace(`${pathname}?${params.toString()}`);
+}, 3000);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
